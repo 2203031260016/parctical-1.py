cd7b57e3dbcf65e68973583c278e0c2f8cfecc74
2203031260016 <157509774+2203031260016@users.noreply.github.com>
2024-01-24 00:20:45 +0530

    practical-1

--- practical1.py
+++ practical1.py
@@ -6,6 +6,6 @@ fh.write("fahrenheit is : "+str(f))
     
 #, °C = (°F - 32) × 5/9  fahrenheit to celsius
 f=int(input("enter fahrenheit value : "))
-c=((f-32)*5/9) #
+c=((f-32)*5/9) 
 fh.write("celsius convertion is:"+str(c))
 
